32af4fcee1dfc011828a7e40e887f97e0dd9b603
simi <simon.marty@bluewin.ch>
2022-11-09 16:39:23 +0100

    feat: Styling power slider and category checkboxes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#FFC700",
+        gray: colors.neutral,
+      },
+      fontFamily: {
+        "albert-sans": ["Albert Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
