2d95689457ed4891b7737ec858c9a1f022683dde
jeremiasjutz <jutz.jeremias@gmail.com>
2022-12-15 13:26:37 +0100

    refactor: tests

--- cypress/e2e/toggleControlPanel.cy.ts
+++ cypress/e2e/toggleControlPanel.cy.ts
@@ -9,13 +9,3 @@ describe("Visit enerCHy and toggle controlpanel", () => {
       .should("contain", "translateX(-100%)");
   });
 });
-
-describe("Visit enerCHy and toggle statistics panel", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:5173/");
-    cy.wait(4500);
-    cy.get("button").contains("Berglandschaft entdecken").click();
-    cy.get("#toggleStatisticsPanelButton").click();
-    cy.get("#statisticsPanel").should("not.exist");
-  });
-});
